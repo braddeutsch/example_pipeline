aaf02a238a9b71dc542c99b0f2b2fa593c9db11d
Brad Deutsch <brad@schireson.com>
2018-04-26 12:27:13 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Modified template of Cookiecutter Data Science repo',
+    author='Brad Deutsch',
+    license='MIT',
+)
